b595e91a8d3516c3558dd204e0045ea592e9d085
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-01-26 17:50:06 +0200

    git init

---
+++ src/api/apiSlice.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BASE_API_URL = import.meta.env.VITE_API_URL; 
+
+interface User {
+   id: number;
+   username: string;
+   email: string;
+   password: string;
+   age: number;
+}
+
+export const apiSlice = createApi({
+   reducerPath: "api",
+   baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }),
+   endpoints: (builder) => ({
+      getAllUsers: builder.query<User[], void>({
+         query: () => `/all-users`,
+      }),
+   }),
+});
+
+export const { useGetAllUsersQuery } = apiSlice;
